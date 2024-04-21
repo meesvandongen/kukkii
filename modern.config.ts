1ca32005e679ac40430b13d680c36b2afd88c6b2
Mees van Dongen <meesvandongen@gmail.com>
2024-04-21 16:22:11 +0200

    workflow

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,21 @@
-import { moduleTools, defineConfig } from '@modern-js/module-tools';
+import { moduleTools, defineConfig } from "@modern-js/module-tools";
 
 export default defineConfig({
   plugins: [moduleTools()],
-  buildPreset: 'npm-library',
+  buildConfig: [
+    {
+      format: "cjs",
+      target: "es2022",
+      buildType: "bundleless",
+      outDir: "./dist/lib",
+      sourceMap: true,
+    },
+    {
+      format: "esm",
+      target: "es2022",
+      buildType: "bundleless",
+      outDir: "./dist/es",
+      sourceMap: true,
+    },
+  ],
 });
